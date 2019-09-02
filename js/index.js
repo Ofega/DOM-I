f503ec95f30138c54a826d4abf81fd2f1afb09ab
ofega <fegaeze@gmail.com>
2019-09-02 12:38:14 +0100

    inserted images into the dom

--- js/index.js
+++ js/index.js
@@ -40,3 +40,10 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Task 1:
+let ctaImage = document.getElementById("cta-img");
+ctaImage.setAttribute('src', siteContent["cta"]["img-src"])
+
+let middleImage = document.getElementById("middle-img");
+middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"])
