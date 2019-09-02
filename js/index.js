ef2f809c0910fbc729a21c62700a8ef16155ceca
ofega <fegaeze@gmail.com>
2019-09-02 16:17:42 +0100

    refactored index.js

--- js/index.js
+++ js/index.js
@@ -42,101 +42,103 @@ const siteContent = {
 };
 
 
-// Images Selector
-const logo = document.getElementById("logo-img");
-const ctaImage = document.getElementById("cta-img");
-const middleImage = document.getElementById("middle-img");
+function setImage() {
+  const logo = document.getElementById("logo-img");
+  const ctaImage = document.getElementById("cta-img");
+  const middleImage = document.getElementById("middle-img");
 
-// Navigation Selector
-const navLinks = document.querySelectorAll('nav a');
-const nav = document.querySelector('nav');
-
-// CTA Selector
-const ctaContentHeader = document.querySelector(".cta-text h1");
-const ctaContentButton = document.querySelector(".cta-text button"); 
-
-// Main Content Selector
-const mainContentArray = document.querySelectorAll(".text-content");
-
-// Contact Selector
-const contactHeading = document.querySelector('.contact h4');
-const contactTexts = document.querySelectorAll('.contact p');
-
-// Footer Selector
-const footerText = document.querySelector('footer p');
-
-// Create new a tag to append to nav
-const newLink = document.createElement('a');
-newLink.textContent = 'Contact';
-newLink.setAttribute('href', '#');
-newLink.style.color = 'green';
-
-// Array for the navigation text
-const idArray = ["features", "about", "services", "product", "vision"];
-
-// Is page updated?
-let isUpdated = false;
-
-// Update Content on page load
-// dynamicContentUpdate();
-
-
-// Dynamically update if isToggle is true
-function dynamicContentUpdate() {
   // Set images src
   logo.setAttribute('src', siteContent["nav"]["img-src"])
   ctaImage.setAttribute('src', siteContent["cta"]["img-src"]);
   middleImage.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+}
+
+function createNewLink() {
+  const newLink = document.createElement('a');
+  const nav = document.querySelector('nav');
+
+  newLink.textContent = 'Contact';
+  newLink.setAttribute('href', '#');
+  newLink.style.color = 'green';
+
+  nav.appendChild(newLink);
+}
+
+function setNavigation() {
+  const navLinks = document.querySelectorAll('nav a');
 
   // Show each nav link and give green color
   navLinks.forEach((link, index) => {
     link.textContent = siteContent['nav'][`nav-item-${index}`];
     link.style.color = 'green';
   })
+  
+  createNewLink();
+}
 
-  // Add newly created nav link to navigation
-  nav.appendChild(newLink);
+function updateCTA() {
+  const ctaContentHeader = document.querySelector(".cta-text h1");
+  const ctaContentButton = document.querySelector(".cta-text button"); 
 
   // Update cta content
   ctaContentHeader.innerHTML = `DOM<br> Is<br> Awesome`;
   ctaContentButton.textContent = siteContent['cta']['button'];
+}
+
+function updateMain() {
+  const mainContentArray = document.querySelectorAll(".text-content");
+  const idArray = ["features", "about", "services", "product", "vision"];
 
   // Update content for the children of text content
   mainContentArray.forEach((item, index) => {
     item.children[0].textContent = siteContent['main-content'][`${idArray[index]}-h4`];
     item.children[1].textContent = siteContent['main-content'][`${idArray[index]}-content`];
   })
+}
+
+function updateContact() {
+  const contactHeading = document.querySelector('.contact h4');
+  const contactTexts = document.querySelectorAll('.contact p');
 
   // Update content for contact
   contactHeading.textContent = siteContent['contact']['contact-h4'];
   contactTexts[0].innerHTML = `123 Way 456 Street<br> Somewhere, USA`;
   contactTexts[1].textContent = siteContent['contact']['phone'];
   contactTexts[2].textContent = siteContent['contact']['email'];
+}
+
+function updateFooter() {
+  const footerText = document.querySelector('footer p');
 
   // Update footer content
   footerText.textContent = siteContent['footer']['copyright'];
 }
 
+function init() {
+  let isUpdated = false;
+  const toggleBtn = document.createElement('button');
 
-// === Stretch Goal ===
-const btnContainer = document.createElement('div');
-const toggleBtn = document.createElement('button');
-const clearBtn = document.createElement('button');
+  toggleBtn.style = 'position:fixed;top:0;left:0';
+  toggleBtn.textContent = 'Update Content';
 
+  toggleBtn.addEventListener('click', function() {
+    if(isUpdated) {
+      alert('Page is already updated')
+    } else {
+      setImage();
+      setNavigation();
+      updateCTA();
+      updateMain();
+      updateContact();
+      updateFooter();
+      isUpdated = true;
+    }
+  })
 
-toggleBtn.style = 'position:fixed;top:0;left:0';
-toggleBtn.textContent = 'Update Content';
+  document.body.prepend(toggleBtn);
+}
 
-toggleBtn.addEventListener('click', function() {
-  if(isUpdated) {
-    alert('Page is already updated')
-  } else {
-    dynamicContentUpdate();
-    isUpdated = true;
-  }
-})
-
-document.body.prepend(toggleBtn);
+init();
   
 
 
